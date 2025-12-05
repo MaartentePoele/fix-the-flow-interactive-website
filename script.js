1dbea61c1da690ed06981f052e9dbd0f9f95a4ac
MaartentePoele <maarten.te.poele@hva.nl>
2025-12-05 11:02:17 +0100

    #13 variabele naam aangepast

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 // Selecteer alle buttons
-const buttons = document.querySelectorAll("button");
+const allButtons = document.querySelectorAll("button");
 
 // Selecteer alle artikelen
 const allArticles = document.querySelectorAll("article");
 
 // Kijk of er op een button wordt geklikt
-buttons.forEach((button) => {
+allButtons.forEach((button) => {
   button.addEventListener("click", () => {
     // Sla de waarde van de button op in een variabele
     const doelgroep = button.value;
